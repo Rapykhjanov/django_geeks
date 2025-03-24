b8d19eb0c1d2ff118abccda95c2d41a227430611
Rapykhjanov <rapihzanovsardor@gmail.com>
2025-03-24 15:29:04 +0600

    hw_6

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,27 +1,32 @@
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from django.contrib.auth import login
 from .serializers import RegistrationSerializer, ConfirmSerializer, LoginSerializer
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = RegistrationSerializer(data=request.data)
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
+            serializer.save()
             return Response({"message": "Регистрация успешна. Код подтверждения отправлен."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ConfirmView(APIView):
-    def post(self, request):
-        serializer = ConfirmSerializer(data=request.data)
+class ConfirmView(generics.UpdateAPIView):
+    serializer_class = ConfirmSerializer
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             return Response({"message": "Аккаунт подтвержден."}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class LoginView(APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data
             login(request, user)
